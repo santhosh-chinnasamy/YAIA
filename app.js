3c91cba0fc8a664605f0f444e8432b4c9672c503
Santhosh <csesanthosh15@gmail.com>
2018-08-14 20:07:04 +0530

    add firebase sdk

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDY97uW202INfcJUgftVa0OCVDInDmRAgs",
+    authDomain: "project-invoice-c9c15.firebaseapp.com",
+    databaseURL: "https://project-invoice-c9c15.firebaseio.com",
+    projectId: "project-invoice-c9c15",
+    storageBucket: "project-invoice-c9c15.appspot.com",
+    messagingSenderId: "531611377260"
+  };
+  firebase.initializeApp(config);
