1d866000bccdce32c13b977d9cf4785038c3c1a2
Santhosh <csesanthosh15@gmail.com>
2018-08-15 14:47:28 +0530

    update/delete script

--- app.js
+++ app.js
@@ -31,3 +31,42 @@ reviewForm.addEventListener('submit', (e) => {
     message.value = '';
     hiddenId.value = '';
 });
+
+//read
+
+var reviews = document.getElementById('reviews');
+var reviewsRef = db.ref('/reviews');
+
+reviewsRef.on('child_added', (data) => {
+    var li = data.key;
+    li.innerHTML = reviewTemplate(data.val())
+    reviews.appendChild(li);
+});
+
+reviewsRef.on('child_changed', (data) => {
+    var reviewNode = document.getElementById(data.key);
+    reviewNode.innerHTML = reviewTemplate(data.val());
+});
+
+reviewsRef.on('child_removed', (data) => {
+    var reviewNode = document.getElementById(data.key);
+    reviewNode.parentNode.removeChild(reviewNode);
+});
+
+reviews.addEventListener('click', (e) => {
+    var reviewNode = e.target.parentNode
+
+    //update
+    if (e.target.classList.contains('edit')) {
+        fullname.value = reviewNode.querySelector('.fullname').innerText;
+        message.value = reviewNode.querySelector('.message').innerText;
+        hiddenId.value = reviewNode.id;
+    }
+
+    //delete
+
+    if (e.target.classList.contains('delete')) {
+        var id = reviewNode.id;
+        db.ref('reviews/'+id).remove;
+    }
+});
